30046a381aabfc8ad762f96ece119364d7a3dc98
Yothin Muangsommuk <m@yothinix.com>
2020-12-18 13:07:45 +0700

    Edit Django settings for base local also update manage.py

---
+++ instagram/instagram/settings/local.py
@@ -0,0 +1,16 @@
+from .base import *
+
+ALLOWED_HOSTS = ["*"]
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
